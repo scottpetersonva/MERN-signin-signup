f497f037e19ebf5fdd2cb07d857ad57c3c5a4491
Scott Peterson <scott.peterson.va@gmail.com>
2018-06-26 10:49:54 -0400

    third try

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const historyApiFallback = require('connect-history-api-fallback');
 const mongoose = require('mongoose');
 const path = require('path');
-// const webpack = require('webpack');
-// const webpackDevMiddleware = require('webpack-dev-middleware');
-// const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const config = require('../config/config');
-// const webpackConfig = require('../webpack.config');
+const webpackConfig = require('../webpack.config');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const port  = process.env.PORT || 8080;
@@ -28,37 +28,37 @@ app.use(express.json());
 // API routes
 require('./routes')(app);
 
-// if (isDev) {
-//   const compiler = webpack(webpackConfig);
+if (isDev) {
+  const compiler = webpack(webpackConfig);
 
-//   app.use(historyApiFallback({
-//     verbose: false
-//   }));
+  app.use(historyApiFallback({
+    verbose: false
+  }));
 
-//   app.use(webpackDevMiddleware(compiler, {
-//     publicPath: webpackConfig.output.publicPath,
-//     contentBase: path.resolve(__dirname, '../client/public'),
-//     stats: {
-//       colors: true,
-//       hash: false,
-//       timings: true,
-//       chunks: false,
-//       chunkModules: false,
-//       modules: false
-//     }
-//   }));
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    contentBase: path.resolve(__dirname, '../client/public'),
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  }));
 
-//   app.use(webpackHotMiddleware(compiler));
-//   app.use(express.static(path.resolve(__dirname, '../dist')));
-// } 
+  app.use(webpackHotMiddleware(compiler));
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+} 
 
-// else {
+else {
   app.use(express.static(path.resolve(__dirname, '../dist')));
   app.get('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, '../dist/index.html'));
     res.end();
   });
-// }
+}
 
 app.listen(port, '0.0.0.0', (err) => {
   if (err) {
